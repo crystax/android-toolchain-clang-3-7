226567369eecbe054dc3121b3b550c630fad7535
WenHan Gu <Wenhan.gu@mediatek.com>
2014-02-07 10:49:32 +0800

    [ndk][pndk] Unknown arch support for 64bit. Also, add PIE support.
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

--- lib/CodeGen/TargetInfo.cpp
+++ lib/CodeGen/TargetInfo.cpp
@@ -6255,14 +6255,18 @@ void AndroidABIInfo::computeInfo(CGFunctionInfo &FI) const {
 
 llvm::Value *AndroidABIInfo::EmitVAArg(llvm::Value *VAListAddr, QualType Ty,
                                        CodeGenFunction &CGF) const {
-  CGBuilderTy &Builder = CGF.Builder;
-  llvm::Type *PTy =
-    llvm::PointerType::getUnqual(CGF.ConvertType(Ty));
+  if (getTarget().getTriple().getArch() == llvm::Triple::le64) {
+    return NULL;  // Let it generate llvm::VAArgInst
+  } else {
+    CGBuilderTy &Builder = CGF.Builder;
+    llvm::Type *PTy =
+      llvm::PointerType::getUnqual(CGF.ConvertType(Ty));
 
-  // Since ABIInfo::EmitVAArg should return a pointer of Ty,
-  // We emit va_arg i8** va_list, Ty* for Ty
-  llvm::Value *v = Builder.CreateVAArg(VAListAddr, PTy);
-  return v;
+    // Since ABIInfo::EmitVAArg should return a pointer of Ty,
+    // We emit va_arg i8** va_list, Ty* for Ty
+    llvm::Value *v = Builder.CreateVAArg(VAListAddr, PTy);
+    return v;
+  }
 }
 
 
@@ -7212,6 +7216,9 @@ const TargetCodeGenInfo &CodeGenModule::getTargetCodeGenInfo() {
       return *(TheTargetCodeGenInfo = new AndroidTargetCodeGenInfo(Types));
     else
       return *(TheTargetCodeGenInfo = new PNaClTargetCodeGenInfo(Types));
+  case llvm::Triple::le64:
+    if (Triple.getOS() == llvm::Triple::NDK)
+      return *(TheTargetCodeGenInfo = new AndroidTargetCodeGenInfo(Types));
   case llvm::Triple::mips:
   case llvm::Triple::mipsel:
     if (Triple.getOS() == llvm::Triple::NaCl)
