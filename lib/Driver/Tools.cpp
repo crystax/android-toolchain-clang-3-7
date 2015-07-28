7837d53d7f13aefd3d384bde9d2fe5aa616222dd
Chih-Hung Hsieh <chh@google.com>
2015-07-28 16:27:56 +0000

    Add -femulated-tls flag to select the emulated TLS model.
    This will be used for old targets like Android that do not
    support ELF TLS models.
    
    Differential Revision: http://reviews.llvm.org/D10524
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@243441 91177308-0d34-0410-b5e6-96231b3b80d8
    
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -4130,7 +4130,13 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
   Args.AddLastArg(CmdArgs, options::OPT_fstandalone_debug);
   Args.AddLastArg(CmdArgs, options::OPT_fno_standalone_debug);
   Args.AddLastArg(CmdArgs, options::OPT_fno_operator_names);
-  // AltiVec-like language extensions aren't relevant for assembling.
+  // Emulated TLS is enabled by default on Android, and can be enabled manually
+  // with -femulated-tls.
+  bool EmulatedTLSDefault = Triple.getEnvironment() == llvm::Triple::Android;
+  if (Args.hasFlag(options::OPT_femulated_tls, options::OPT_fno_emulated_tls,
+                   EmulatedTLSDefault))
+    CmdArgs.push_back("-femulated-tls");
+  // AltiVec language extensions aren't relevant for assembling.
   if (!isa<PreprocessJobAction>(JA) || Output.getType() != types::TY_PP_Asm) {
     Args.AddLastArg(CmdArgs, options::OPT_faltivec);
     Args.AddLastArg(CmdArgs, options::OPT_fzvector);
